c5231c694513713e6000ba5ed0be6bc52cd62877
martincalsina <martincalsina05@gmail.com>
2025-08-19 13:21:27 -0300

    first commit

---
+++ src/main/java/com/foro_hub/alura/foro_hub/ForoHubAluraApplication.java
@@ -0,0 +1,13 @@
+package com.foro_hub.alura.foro_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foro_hub/alura/foro_hub/ForoHubAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foro_hub.alura.foro_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
